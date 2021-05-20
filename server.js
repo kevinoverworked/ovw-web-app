c682234390a3f77d9266c553607ee1aa78d9c895
kevinoverworked <kevinoverworked@gmail.com>
2021-05-20 10:41:35 -0700

    Moved server.js, rebuilt react client

--- server.js
+++ server.js
@@ -26,17 +26,17 @@ app.use(bodyParser.urlencoded({
 app.use(cors());
 
 // Require Route
-const api = require("./routes/routes");
+const api = require("./server/src/routes/routes");
 
 //Configure app to use route
 app.use("/api/v1", api);
 
 // This middleware informs the express application to serve our compiled React files
 if (process.env.NODE_ENV === "production" || process.env.NODE_ENV === "staging") {
-    app.use(express.static(path.join(__dirname, "../client/build")));
+    app.use(express.static(path.join(__dirname, "client/build")));
 
     app.get("*", function (req, res) {
-        res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+        res.sendFile(path.join(__dirname, "client/build", "index.html"));
     });
 };
 
